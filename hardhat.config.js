2566cf34ed66b4c815aef795958fd8a8f90d220b
Umair Mirza <umair_hp@hotmail.com>
2022-07-13 23:07:26 +0500

    uncommented the mumbai testnet in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,10 +14,10 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    // mumbai: {
-    //   url: "https://rpc-mumbai.maticvigil.com",
-    //   accounts: [process.env.privateKey]
-    // }
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
   },
   solidity: {
     version: "0.8.4",
